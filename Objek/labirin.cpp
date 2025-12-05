1d24986e5d50ef0505a0407d97a84b80cdeae70f
mahagacor777 <icalnov755@gmail.com>
2025-12-05 16:29:12 +0700

    Update labirin.cpp

--- Objek/labirin.cpp
+++ Objek/labirin.cpp
@@ -26,13 +26,23 @@ const char* kotak1[] = {
         "||                 |                   ||",
         "=========================================",
     };
-int angka;
+ int baris1 = 18;
+    int kolom1 = 38;
 
+    // Hitung posisi tengah layar 15x80
+    int mulaiY1 = (layarTinggi1 - baris1) / 2;
+    int mulaiX1 = (layarLebar1  - kolom1) / 2;
 
+    // Gambar labirin
+    for (int i = 0; i < baris1; i++) {
+        mvprintw(mulaiY1 + i, mulaiX1, "%s", kotak1[i]);
+    }
+}
 
 void tampilkanLabirin2() {
     // Ukuran layar yang ditetapkan
     int layarTinggi2 = 25;
     int layarLebar2  = 80;
+
 
 
