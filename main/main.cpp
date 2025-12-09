637a18f2ffb3d85ca6515a6c537a727e6eb73778
mahagacor777 <icalnov755@gmail.com>
2025-12-09 15:16:27 +0700

    Refactor main.cpp to include character functions

--- main/main.cpp
+++ main/main.cpp
@@ -1,13 +1,38 @@
+
 #include <iostream>
 using namespace std;
 
-// ==== Deklarasi fungsi dari folder Objek ====
-void introOpening();
-void introEnding();
+//  Deklarasi fungsi dari folder Objek 
+void karakter_opening();
+void karakter_ending();
 void tampilLabirin();
 
-// ==== Deklarasi fungsi dari folder Sistem game ====
+// fungsi dari folder Sistem game
 void modeEasy();
 void modeMedium();
 void modeHard();
+
+int main() {
+    int pilih;
+
+   karakter_opening();   
+
+    cout << "\n=== GAME LABIRIN ===\n";
+    cout << "1. Easy\n";
+    cout << "2. Medium\n";
+    cout << "3. Hard\n";
+    cout << "Pilih mode: ";
+    cin >> pilih;
+
+    switch (pilih) {
+    case 1: modeEasy(); break;
+    case 2: modeMedium(); break;
+    case 3: modeHard(); break;
+    default:
+        cout << "Pilihan tidak valid!\n";
+    }
+
+    karakter_ending();
+    return 0;
+}
 
