0b0473a93a3cb943ca9a1d0fee03676be7559726
mahagacor777 <icalnov755@gmail.com>
2025-12-09 15:08:28 +0700

    Add function declarations for game modes and intro

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// ==== Deklarasi fungsi dari folder Objek ====
+void introOpening();
+void introEnding();
+void tampilLabirin();
+
+// ==== Deklarasi fungsi dari folder Sistem game ====
+void modeEasy();
+void modeMedium();
+void modeHard();
+
