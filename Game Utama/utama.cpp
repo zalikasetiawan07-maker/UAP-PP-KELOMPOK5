266346c2c9aaed835bce0b6c5e232c5e5c679242
mahagacor777 <icalnov755@gmail.com>
2025-12-13 22:57:17 +0700

    Implement menu navigation in utama.cpp
    Add menu navigation functionality using keyboard input.

--- Game Utama/utama.cpp
+++ Game Utama/utama.cpp
@@ -15,3 +15,37 @@ int menuUtama() {
 
     int jumlahMenu = 4;
     int pilihan = 0;
+keypad(stdscr, TRUE);
+    nodelay(stdscr, FALSE);
+
+    while (true) {
+        clear();
+mvprintw(5, 10, "===== GAME LABIRIN =====");
+        mvprintw(6, 10, "Gunakan UP / DOWN lalu ENTER");
+
+        for (int i = 0; i < jumlahMenu; i++) {
+     if (i == pilihan) {
+                attron(A_REVERSE);
+        mvprintw(8 + i, 10, "> %s", menu[i]);
+                     attroff(A_REVERSE);
+            } else {
+                mvprintw(8 + i, 10, "  %s", menu[i]);
+            }
+        }
+
+        refresh();
+  int ch = getch();
+        if (ch == KEY_UP) {
+            pilihan = (pilihan - 1 + jumlahMenu) % jumlahMenu;
+        }
+        else if (ch == KEY_DOWN) {
+            pilihan = (pilihan + 1) % jumlahMenu;
+        }
+        else if (ch == '\n') {
+            return pilihan;
+        }
+    }
+}
+
+
+
