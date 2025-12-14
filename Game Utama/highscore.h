db81064538b49d0e86f39cec09da7b0c965556cf
mahagacor777 <icalnov755@gmail.com>
2025-12-14 16:45:36 +0700

    Add highscore.h for managing high scores

---
+++ Game Utama/highscore.h
@@ -0,0 +1,9 @@
+#ifndef HIGHSCORE_H
+#define HIGHSCORE_H
+
+#include <string>
+
+int bacaHighScore(const std::string& namaFile);
+void simpanHighScore(const std::string& namaFile, int skorBaru);
+
+#endif
