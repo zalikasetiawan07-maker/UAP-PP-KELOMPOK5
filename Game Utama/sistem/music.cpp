a30f3b42e8b93681712f9653ab70baed21ad3c37
mahagacor777 <icalnov755@gmail.com>
2025-12-14 20:58:51 +0700

    Update music.cpp

--- Game Utama/sistem/music.cpp
+++ Game Utama/sistem/music.cpp
@@ -1,1 +1,12 @@
+#include "music.h"
+#include <cstdlib>
+
+void mainkanMusik() {
+    std::string perintah = "mpg123 -q \"" + FILE_MUSIK + "\" &"; 
+    system(perintah.c_str());
+}
+
+void hentikanMusik() {
+    system("killall mpg123 2>/dev/null");
+}
 
