eb9014b4991adfae60c8d6385b997f06bca0ce72
mahagacor777 <icalnov755@gmail.com>
2025-12-14 20:32:20 +0700

    Add winning and losing animations to ending.cpp

--- Game Utama/opening ending/ending.cpp
+++ Game Utama/opening ending/ending.cpp
@@ -2,14 +2,42 @@
 #include <ncursesw/curses.h>
 #include <unistd.h>
 
-void tampilEnding(int skor) {
+void animasiMenang(int posY, int posX) {
+    beep();    
+    const char* teks[] = {
+        " _   _ _____ _____ _______ ______ ______   _    _  ",
+        "| | | |_   _|  ___|__   __|  __  |  __  | | |  | | ",
+        "| | | | | | | |      | |  | |  | | |__| | | |__| | ",
+        "| | | | | | | |      | |  | |  | |  __  | |____  | ",
+        "| |_| |_| |_| |___   | |  | |__| | |  | |_ ____| | ",
+        "|_____|_____|_____|  |_|  |______|_|  |___|______| "
+    };
+
     clear();
+    for (int i = 0; i < 6; i++) {
+        mvprintw(posY + i, posX, "%s", teks[i]);
+        refresh();
+        usleep(120000);
+    }
+    sleep(2);
+}
 
-    mvprintw(5, 10, "TERIMA KASIH TELAH BERMAIN!");
-    mvprintw(7, 10, "Skor Akhir : %d", skor);
-    mvprintw(9, 10, "Dibuat oleh:");
-    mvprintw(10, 10, "Nama Kamu");
+void animasiKalah(int posY, int posX) {
+    beep(); 
+    const char* teks[] = {
+        " _____   _____  ____  _____  ______  _______  ",
+        "|  __ \\ |  ___||  __||  ___||  __  ||__   __| ",
+        "| |  | || |__  | |__ | |___ | |__| |   | |    ",
+        "| |  | ||  __| |  __||  ___||  __  |   | |    ",
+        "| |__| || |___ | |   | |___ | |  | |   | |    ",
+        "|_____/ |_____||_|   |_____||_|  |_|   |_|    "
+    };
 
-    refresh();
-    sleep(3);
+    clear();
+    for (int i = 0; i < 6; i++) {
+        mvprintw(posY + i, posX, "%s", teks[i]);
+        refresh();
+        usleep(120000);
+    }
+    sleep(2);
 }
