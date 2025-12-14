d0f6abdc05ccac7846b1a687b9180a2e96fcd007
mahagacor777 <icalnov755@gmail.com>
2025-12-14 20:19:45 +0700

    Add high score read and write functions

--- Game Utama/sistem/highscore.h
+++ Game Utama/sistem/highscore.h
@@ -1,1 +1,11 @@
+#ifndef HIGHSCORE_H
+#define HIGHSCORE_H
+
+#include <string>
+
+// Fungsi baca dan tulis skor untuk tiap level
+bool bacaSkor(const std::string &namaFile, int &nilai);
+bool tulisSkor(const std::string &namaFile, int nilai);
+
+#endif
 
